d1650ad44225ac2b4760dff99ab9debe3b9f06d7
cshah <cshah@tavisca.com>
2017-03-05 23:07:15 +0530

    Initial Commit

---
+++ CountingSubFoldersAndFilesInDirectory/DirectoryManagar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingSubFoldersAndFilesInDirectory
+{
+    class DirectoryManagar
+    {
+        public void DisplayFoldersAndFiles(string path)
+        {
+            int directoryCount = Directory.GetDirectories(@path, "*.*", SearchOption.AllDirectories).Length;
+            Console.WriteLine("Total Number of Folder : " + directoryCount);
+
+            int fileCount = Directory.GetFiles(@path, "*.*", SearchOption.AllDirectories).Length;
+            Console.WriteLine("Total Number of Files : " + fileCount);
+        }
+    }
+}
---
+++ CountingSubFoldersAndFilesInDirectory/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingSubFoldersAndFilesInDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Folder Path : ");
+            String path = Console.ReadLine();
+            DirectoryManagar directoryManager = new DirectoryManagar();
+            directoryManager.DisplayFoldersAndFiles(path);
+            Console.ReadKey();
+        }
+    }
+}
